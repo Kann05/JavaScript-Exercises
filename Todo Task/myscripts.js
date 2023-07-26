6de93f113632afe3861cda6f53f9d51cd633c5e4
Erkan <zapochvamdneska@gmail.com>
2023-07-26 23:28:32 +0300

    Project

--- Todo Task/myscripts.js
+++ Todo Task/myscripts.js
@@ -9,6 +9,7 @@ window.addEventListener("load", function () {
     arrayDateSave = parsedTasks.dates;
     DisplayToDoTask();
   }
+  ChangeColors();
 });
 
 function DisplayToDoTask() {
@@ -17,16 +18,55 @@ function DisplayToDoTask() {
   for (let index = 0; index < arrayTodoSave.length; index++) {
     const todoTask = arrayTodoSave[index];
     const todoDate = arrayDateSave[index];
-    let html = `<p style="display: grid; grid-template-columns: repeat(4, minmax(0, 1fr)); column-gap: 5px; margin-right:auto; margin-top: 50px;">
+    let html = `<p style="display: grid; grid-template-columns: repeat(5, minmax(0, 1fr)); column-gap: 5px; margin-right:auto; margin-top: 50px;">
         Task ${index + 1}:
         <span class="Todo-name">${todoTask}</span>
         <span class="Todo-date">${todoDate}</span>
+        <select class="statusSelect">
+          <option class="select" value="select">Select...</option>
+          <option class="done" value="done">Done</option>
+          <option class="inprogress" value="In Progress">In Progress</option>
+          <option class="pending" value="Pending">Pending</option>
+        </select>
         <button onclick="RemoveTask(${index})" class="delete-button">Delete</button>
     </p>`;
     toDoHTML += html;
   }
 
   let DisplayTask = (document.querySelector("#tasks").innerHTML = toDoHTML);
+}
+
+function ChangeColors() {
+  let tasksContainer = document.querySelector("#tasks");
+
+  tasksContainer.addEventListener("change", function (event) {
+    let selectedValue = event.target.value;
+
+    if (selectedValue === "In Progress") {
+      event.target.style.backgroundColor = "#3498DB";
+      event.target.style.color = "white";
+      event.target.style.border = "none";
+      event.target.style.fontWeight = "bold";
+    }
+    if (selectedValue === "Pending") {
+      event.target.style.backgroundColor = "#FFA500";
+      event.target.style.color = "white";
+      event.target.style.border = "none";
+      event.target.style.fontWeight = "bold";
+    }
+    if (selectedValue === "select") {
+      event.target.style.backgroundColor = "white";
+      event.target.style.color = "black";
+      event.target.style.border = "none";
+      event.target.style.fontWeight = "bold";
+    }
+    if (selectedValue === "done") {
+      event.target.style.backgroundColor = "#008000";
+      event.target.style.color = "white";
+      event.target.style.border = "none";
+      event.target.style.fontWeight = "bold";
+    }
+  });
 }
 
 function RemoveTask(index) {
