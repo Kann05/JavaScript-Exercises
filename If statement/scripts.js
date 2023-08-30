5fc3b8514c6583b403e43cc81fc0498e3d5c6309
Erkan <zapochvamdneska@gmail.com>
2023-08-30 16:20:38 +0300

    n

--- If statement/scripts.js
+++
@@ -1,8 +0,0 @@
-let userAge = prompt("Enter your age: ");
-
-if (userAge >= 18){
-    console.log('You can drink alcohol')
-}
-else {
-    console.log('You cant drink alcohol')
-}
--- Variables/script.js
+++
@@ -1,3 +0,0 @@
-let cartquantity = 0;
-
-
